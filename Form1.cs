d0ba8d123e9e15094b40638d91f89a1ef07e8c8e
Akio Orihara <akio.orihara@outlook.com>
2021-07-11 13:03:04 -0500

    completed with another way

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,34 @@ namespace VowelCounterVer2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int counter_x = 0, counter_y = 0;
+            string words = textBox1.Text;
+            for (int i = 0 ; i < words.Length; i++)
+            {
+                if(words[i] == 'a' || words[i] == 'i' || words[i] == 'u' || words[i] == 'e' || words[i] == 'o')
+                {
+                    counter_x += 1; 
+                }
+                if (checkBox1.Checked && words[i] == 'y')
+                    counter_y += 1;
+            }
+
+
+            if (counter_x > 0 && checkBox1.Checked)
+                if (counter_y > 0)
+                    MessageBox.Show("Vowles found :" + counter_x + "\n Y found :" + counter_y + "\n" + "in " + words);
+                else
+                    MessageBox.Show("Vowles found :" + counter_x + "\n Y not found." + "\n" + "in " + words);
+            else if (counter_x == 0 && checkBox1.Checked)
+                MessageBox.Show("Vowles not found, but" + " Y count is : " + counter_y + "\n" + "in " + words);
+            else if (counter_x > 0)
+                MessageBox.Show("Vowles found : " + counter_x + "\n" + "in " + words);
+            else
+                MessageBox.Show("No Vowels found" + "\n" + "in " + words);
+
+        }
     }
 }
